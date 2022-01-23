92a301ce82ed0d66f11ac355c55fd53e824a70aa
Chrome Villager <chr0mevillager@gmail.com>
2022-01-23 14:55:30 -0700

    Added in a /feedback command

--- src/commands.ts
+++ src/commands.ts
@@ -8,6 +8,7 @@ import {
     CacheType,
     MessageEmbed,
     ColorResolvable,
+    User,
 } from 'discord.js';
 import { SlashCommandBuilder }  from "@discordjs/builders";
 dotenv.config();
@@ -19,35 +20,96 @@ interface CustomCommand {
 }
 
 let commands: Record<string, CustomCommand> = {
-    help: {
+    //
+    feedback: {
         data: {
-            name: "help",
-            description: "See documentation, credits, and our help server!",
+            name: "feedback",
+            description: "Send us some feedback about Bible verse recommendations, bugs, and new ideas!",
             options: [
                 {
-                    name: "my-arg",
-                    description: "AAAAAHHHH!!!",
-                    type: "STRING",
-                    required: true,
+                    name: "about",
+                    description: "What do you want to send feedback about?",
+                    type: "SUB_COMMAND_GROUP",
+                    options: [
+                        {
+                            name: "verse",
+                            description: "Send us a bible verse that you think would be a great daily scripture!",
+                            type: "SUB_COMMAND",
+                            options: [
+                                {
+                                    name: "verse",
+                                    description: "What verse do you recommend?",
+                                    type: "STRING",
+                                    required: true,
+                                },
+                            ],
+                        },
+                        {
+                            name: "bug",
+                            description: "Report a bug to us!",
+                            type: "SUB_COMMAND",
+                            options: [
+                                {
+                                    name: "bug-description",
+                                    description: "What bug did you find?",
+                                    type: "STRING",
+                                    required: true,
+                                },
+                            ],
+                        },
+                        {
+                            name: "idea",
+                            description: "Recommend a new idea for the bot!",
+                            type: "SUB_COMMAND",
+                            options: [
+                                {
+                                    name: "idea-description",
+                                    description: "What idea did you have?",
+                                    type: "STRING",
+                                    required: true,
+                                },
+                            ],
+                        },
+                    ],
                 }
             ],
         },
         async execute(interaction) {
+            interaction.commandName
+            let content = "Your suggestion is:\n"
+            //console.log(interaction.options.get("verse", true));
+            if (interaction.options.getSubcommand() === "verse") {
+                content += interaction.options.get("verse", true).value! as string;
+            } else if (interaction.options.getSubcommand() === "bug") {
+                content += interaction.options.get("bug-description", true).value! as string;
+            } else if (interaction.options.getSubcommand() === "idea") {
+                content += interaction.options.get("idea-description", true).value! as string;
+            }
+
+            let userID = interaction.user.id;
+            let guildID = interaction.guildId;
+            let guildSize = interaction.guild.memberCount;
+            let interactionID = interaction.id;
+            //Send embed
             await interaction.reply({
                 //content: "Pong!",
                 embeds: [
                     new MessageEmbed()
-                        .setAuthor({
-                            name: "A person (totally not a bot trying to conquer the world)",
+                        .setColor("#389af0")
+                        .setTitle("Feedback has been sent!")
+                        .setDescription(content)
+                        .setFooter({
+                            text: "Server ID: " + guildID + "\n"
+                            + "User ID: " + userID + "\n"
+                            + "Server Size: " + guildSize + "\n"
+                            + "Message ID: " + interactionID,
                         })
-                        .setColor(interaction.options.get("my-arg", true).value! as ColorResolvable)
-                        .setTitle('Help')
-                        .setDescription('Use a slash command its not that hard nerd')
                 ],
                 ephemeral: true,
             });
         },
     },
+    //
 };
 
 export default commands;
--- src/index.ts
+++ src/index.ts
@@ -19,38 +19,6 @@ interface CustomCommand {
     execute(interaction: CommandInteraction<CacheType>): void | Promise<void>;
 }
 
-// let commands: Record<string, CustomCommand> = {
-//     help: {
-//         data: {
-//             name: "help",
-//             description: "See documentation, credits, and our help server!",
-//             options: [
-//                 {
-//                     name: "my-arg",
-//                     description: "AAAAAHHHH!!!",
-//                     type: "STRING",
-//                     required: true,
-//                 }
-//             ],
-//         },
-//         async execute(interaction) {
-//             await interaction.reply({
-//                 //content: "Pong!",
-//                 embeds: [
-//                     new MessageEmbed()
-//                         .setAuthor({
-//                             name: "A person (totally not a bot trying to conquer the world)",
-//                         })
-//                         .setColor(interaction.options.get("my-arg", true).value! as ColorResolvable)
-//                         .setTitle('Help')
-//                         .setDescription('Use a slash command its not that hard nerd')
-//                 ],
-//                 ephemeral: true,
-//             });
-//         },
-//     },
-// };
-
 client.on("interactionCreate", async (interaction) => {
     if(interaction.isCommand()) {
         const command = commands[interaction.commandName];
@@ -59,7 +27,7 @@ client.on("interactionCreate", async (interaction) => {
         } catch(err) {
             console.error(err);
             await interaction.reply({
-                content: "There was an error executing this command",
+                content: "You just found an ultra-rare bug! Please report this by using the \"/feedback\" command, or try again.",
                 ephemeral: true,
             });
         }
