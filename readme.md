fe8c95dbdcf208a00240095dd37940c5f54a7361
Chrome Villager <chr0mevillager@gmail.com>
2022-01-23 15:58:24 -0700

    Added a /help command

--- readme.md
+++ readme.md
@@ -3,12 +3,14 @@
 ## Add To Servers
 [Click here to add the bot to a server](https://discord.com/oauth2/authorize?client_id=932020112733642814&scope=bot%20applications.commands&permissions=8)
 
+---
 ## Use
 ```bash
 	yarn serve
 ```
-
+---
 ## Documentation
 [Click here to see the docs](https://discord.js.org/#/)
 
-[Message Embed Docs](https://discordjs.guide/popular-topics/embeds.html#embed-preview)
+- [Message Embed Docs](https://discordjs.guide/popular-topics/embeds.html#embed-preview)
+- [Slash Command Docs](https://discordjs.guide/interactions/replying-to-slash-commands.html#command-options)
--- src/commands.ts
+++ src/commands.ts
@@ -20,6 +20,48 @@ interface CustomCommand {
 }
 
 let commands: Record<string, CustomCommand> = {
+        //
+        help: {
+            data: {
+                name: "help",
+                description: "See documentation about my commands and find my support server!",
+            },
+            async execute(interaction) {
+                await interaction.reply({
+                    embeds: [
+                        new MessageEmbed()
+                            .setColor("#389af0")
+                            .setTitle("Command help:")
+                            .addFields(
+                                {
+                                    name: "`/info`",
+                                    value: "This command gives you information about me!"
+                                },
+                                { name: '\u200B', value: '\u200B' },
+                                {
+                                    name: "`/feedback` `Verse | Idea | Bug` `< Your Suggestion >`",
+                                    value: "Use this command to send feedback to my developers! You can submit ideas for new features as well as verses and to report bugs. Use the appororiate command for your feedback (`Verse | Idea | Bug`), then type in `< Your Suggestion >`"
+                                },
+                                { name: '\u200B', value: '\u200B' },
+                                {
+                                    name: "`/setup` `< Channel >` `[ Time ]`",
+                                    value: "Use this command to set me up! Set the `< Channel >` to whatever channel you want me to post scriptures in, and optionally set the `< Time >` of day that you would like the scripture to be sent (in UTC)."
+                                },
+                            )
+                    ],
+                    ephemeral: true,
+                });
+                await interaction.followUp({
+                    embeds: [
+                        new MessageEmbed()
+                            .setColor("#389af0")
+                            .setTitle("Other help:")
+                            .setDescription("If you could not find what you were looking for in the message above, you can ask a question in my [Support Server](https://google.com)!")
+                    ],
+                    ephemeral: true,
+                });
+            },
+        },
     //
     feedback: {
         data: {
