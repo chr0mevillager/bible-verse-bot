134af64637d56f221100beb773e4c72cf39e804a
Chrome Villager <chr0mevillager@gmail.com>
2022-02-05 15:12:04 -0700

    Added a small sample of verses.

---
+++ src/exports/verses.ts
@@ -0,0 +1,48 @@
+// {
+// 	title: "",
+// 	translation: "",
+// 	text: "",
+// },
+
+export array verses = [
+	{
+		title: "Isaiah 40:31",
+		translation: "NIV",
+		text: "But those who hope in the Lord will renew their strength. They will soar on wings like eagles; they will run and not grow weary, they will walk and not be faint.",
+	},
+	{
+		title: "1 Samuel 2:1",
+		translation: "NLT",
+		text: "My heart rejoices in the Lord! The Lord has made me strong. Now I have an answer for my enemies; I rejoice because you rescued me.",
+	},
+	{
+		title: "Joshua 1:9",
+		translation: "NLT",
+		text: "This is my command—strong and courageous! Do not be afraid or discouraged. For the Lord your God is with you wherever you go.",
+	},
+	{
+		title: "Hebrews 13:8",
+		translation: "TPT",
+		text: "Jesus, the Anointed One, is always the same—yesterday, today, and forever.",
+	},
+	{
+		title: "Philippians 4:19",
+		translation: "NIV",
+		text: "And my God will meet all your needs according to the riches of his glory in Christ Jesus.",
+	},
+	{
+		title: "Leviticus 19:18",
+		translation: "NIV",
+		text: "Do not seek revenge or bear a grudge against anyone among your people, but love your neighbor as yourself. I am the Lord.",
+	},
+	{
+		title: "Philippians 1:21",
+		translation: "NIV",
+		text: "For me, to live is Christ and to die is gain.",
+	},
+	{
+		title: "Proverbs 19:21",
+		translation: "TPT",
+		text: "Your words are so powerful that they will kill or give life, and the talkative person will reap the consequences.",
+	},
+]
