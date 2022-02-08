a9c30bb08cef0724ec1779e6798e7dfca7197a81
Chrome Villager <chr0mevillager@gmail.com>
2022-02-07 19:17:47 -0700

    Added more verses

--- src/exports/verses.ts
+++ src/exports/verses.ts
@@ -6,6 +6,11 @@
 
 let verses = [
 	{
+		title: "Philippians 4:19",
+		translation: "NIV",
+		text: "And my God will meet all your needs according to the riches of his glory in Christ Jesus.",
+	},
+	{
 		title: "Isaiah 40:31",
 		translation: "NIV",
 		text: "But those who hope in the Lord will renew their strength. They will soar on wings like eagles; they will run and not grow weary, they will walk and not be faint.",
@@ -16,19 +21,24 @@ let verses = [
 		text: "My heart rejoices in the Lord! The Lord has made me strong. Now I have an answer for my enemies; I rejoice because you rescued me.",
 	},
 	{
+		title: "Psalms 73:26",
+		translation: "NIV",
+		text: "My health may fail, and my spirit may grow weak, but God remains the strength of my heart; he is mine forever."
+	}
+	{
 		title: "Joshua 1:9",
 		translation: "NLT",
 		text: "This is my command—strong and courageous! Do not be afraid or discouraged. For the Lord your God is with you wherever you go.",
 	},
 	{
+		title: "Proverbs 27:17",
+		translation: "NLT",
+		text: "As iron sharpens iron, so a friend sharpens a friend.",
+	}
+	{
 		title: "Hebrews 13:8",
 		translation: "TPT",
 		text: "Jesus, the Anointed One, is always the same—yesterday, today, and forever.",
-	},
-	{
-		title: "Philippians 4:19",
-		translation: "NIV",
-		text: "And my God will meet all your needs according to the riches of his glory in Christ Jesus.",
 	},
 	{
 		title: "Leviticus 19:18",
@@ -45,6 +55,16 @@ let verses = [
 		translation: "TPT",
 		text: "Your words are so powerful that they will kill or give life, and the talkative person will reap the consequences.",
 	},
+	{
+		title: "1 Thessalonians 5:17",
+		translation: "TPT",
+		text: "Make your life a prayer."
+	},
+	{
+		title: "Proverbs 14:21",
+		translation: "NIV",
+		text: "It is a sin to despise one's neighbor, but blessed is the one who is kind to the needy.",
+	}
 ]
 
 export default verses;
