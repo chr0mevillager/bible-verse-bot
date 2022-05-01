2fbbc0efc1d4d82fdc9949945a8921e6cd3ddf3a
Chrome Villager <chr0mevillager@gmail.com>
2022-05-01 16:14:23 -0600

    Added more verses

--- readme.md
+++ readme.md
@@ -1,14 +1,19 @@
 # Bible Verses Bot
 
 ## Add To Servers
+
 [Click here to add the bot to a server!](https://discord.com/oauth2/authorize?client_id=932020112733642814&scope=bot%20applications.commands&permissions=8)
 
 ---
+
 ## Use
+
 - To run:
+
 ```bash
-	yarn serve
+ yarn serve
 ```
+
 - [Discord JS Docs](https://discord.js.org/#/)
 - [Message Embed Docs](https://discordjs.guide/popular-topics/embeds.html#embed-preview)
 - [Slash Command Docs](https://discordjs.guide/interactions/replying-to-slash-commands.html#command-options)
--- src/exports/verses.ts
+++ src/exports/verses.ts
@@ -6,9 +6,24 @@
 
 let verses = [
 	{
+		title: "1 Samuel 2:1",
+		translation: "NLT",
+		text: "My heart rejoices in the Lord! The Lord has made me strong. Now I have an answer for my enemies; I rejoice because you rescued me.",
+	},
+	{
+		title: "Proverbs 14:8",
+		translation: "TPT",
+		text: "For the wisdom of the wise will keep life on the right track, while to fool only decieves himself and refuses to face reality.",
+	},
+	{
 		title: "Philippians 4:19",
 		translation: "NIV",
 		text: "And my God will meet all your needs according to the riches of his glory in Christ Jesus.",
+	},
+	{
+		title: "Ephesians 5:15",
+		translation: "NLT",
+		text: "So be careful how you live. Don't live like fools, but like those who are wise.",
 	},
 	{
 		title: "Isaiah 40:31",
@@ -16,9 +31,9 @@ let verses = [
 		text: "But those who hope in the Lord will renew their strength. They will soar on wings like eagles; they will run and not grow weary, they will walk and not be faint.",
 	},
 	{
-		title: "1 Samuel 2:1",
+		title: "Proverbs 3:6",
 		translation: "NLT",
-		text: "My heart rejoices in the Lord! The Lord has made me strong. Now I have an answer for my enemies; I rejoice because you rescued me.",
+		text: "Seek his will in all you do, and he will show you which path to take.",
 	},
 	{
 		title: "Psalms 73:26",
@@ -36,6 +51,11 @@ let verses = [
 		text: "As iron sharpens iron, so a friend sharpens a friend.",
 	},
 	{
+		title: "Romans 5:3",
+		translation: "NLT",
+		text: "We can rejoice, too, when we run into problems and trials, for we know that they help us develop endurance.",
+	},
+	{
 		title: "Hebrews 13:8",
 		translation: "TPT",
 		text: "Jesus, the Anointed One, is always the same—yesterday, today, and forever.",
@@ -44,6 +64,11 @@ let verses = [
 		title: "Leviticus 19:18",
 		translation: "NIV",
 		text: "Do not seek revenge or bear a grudge against anyone among your people, but love your neighbor as yourself. I am the Lord.",
+	},
+	{
+		title: "Proverbs 19:21",
+		translation: "TPT",
+		text: "A person may have many ideas concerning God's plan for life, but only the designs of God's purpose will suceed in the end.",
 	},
 	{
 		title: "Philippians 1:21",
@@ -56,6 +81,11 @@ let verses = [
 		text: "Your words are so powerful that they will kill or give life, and the talkative person will reap the consequences.",
 	},
 	{
+		title: "James 5:13",
+		translation: "NLT",
+		text: "Are any of you suffering hardships? You should pray. Are any of you happy? You should sing praises.",
+	},
+	{
 		title: "1 Thessalonians 5:17",
 		translation: "TPT",
 		text: "Make your life a prayer."
@@ -64,7 +94,62 @@ let verses = [
 		title: "Proverbs 14:21",
 		translation: "NIV",
 		text: "It is a sin to despise one's neighbor, but blessed is the one who is kind to the needy.",
-	}
+	},
+	{
+		title: "Romans 8:28",
+		translation: "NLT",
+		text: "And we know that God causes everything to work together for the good of those who love God and are called according to his purpose for them.",
+	},
+	{
+		title: "Psalms 90:12",
+		translation: "TPT",
+		text: "Help us to remember that our days are numbered, and help us to interpret our lives correctly. Set your wisdom deeply in our hearts so that we may accept your correction.",
+	},
+	{
+		title: "Joshua 1:5",
+		translation: "NLT",
+		text: "No one will be able to stand against you as long as you live. For I will be with you as I was with Moses. I will be with you as I was with Moses. I will not fail you or abandon you.",
+	},
+	{
+		title: "1 John 3:2",
+		translation: "NIV",
+		text: "Dear friends, now we are children of God, and what we will be has not yet been made known. But we know that when Christ appears, we shall be like him, for we shall see him as he is.",
+	},
+	{
+		title: "1 Peter 3:15",
+		translation: "NLT",
+		text: "Instead, you must worship Christ as Lord of your life. And if someone asks about your hope as a beliver, always be ready to explain it.",
+	},
+	{
+		title: "2 Samuel 22:3",
+		translation: "NLT",
+		text: "my God is my rock, in whom I find protection. He is my sheild, the power that saves me, and my place of saftey. He is my refuge, my savior, the one who saves me from violence.",
+	},
+	{
+		title: "Matthew 21:21",
+		translation: "TPT",
+		text: "Jesus replied, \"Listen to the truth. If you do not doubt God's power and speak out of faith's fullness, you can also speak to a tree and it will wither away. Even more than that, you could say to this mountian, 'Be lifted up and be thrown into the sea' and it will be done\"",
+	},
+	{
+		title: "Ephesians 5:15",
+		translation: "NLT",
+		text: "So be careful how you live. Don't live like fools, but like those who are wise.",
+	},
+	{
+		title: "2 Samuel 11:12",
+		translation: "NLT",
+		text: "God is our refuge and strength, always ready to help in times of trouble.",
+	},
+	{
+		title: "Colossians 3:2",
+		translation: "TPT",
+		text: "Yes, feast on all the treasures of the heavenly realm and fill your thoughts with heavenly realities, and not with the distractions of the natural realm.",
+	},
+	{
+		title: "Proverbs 3:5",
+		translation: "NLT",
+		text: "Trust in the Lord with all your heart; do not depend on your own understanding.",
+	},
 ]
 
 export default verses;
